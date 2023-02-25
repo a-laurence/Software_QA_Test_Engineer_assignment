138d6703e95775a30b296c0a83634c927c070fdb
Laurence P. Abanes <laurence@Laurences-MacBook-Air.local>
2023-02-25 15:14:29 +0900

    Added pre-commit config

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ pip3 install --upgrade pip
 pip3 install --user pipenv
 python3 -m pipenv install --dev --skip-lock
 python3 -m pipenv run pre-commit install
+python3 -m pipenv run python version_updater/version_updater.py
