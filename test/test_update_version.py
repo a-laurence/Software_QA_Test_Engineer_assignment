6c636b26d42258ba2154caef176187f1d0ab0772
Laurence P. Abanes <laurence@Laurences-MacBook-Air.local>
2023-02-28 23:06:04 +0900

    Added test_simple_update_replace_value_only()

--- test/test_update_version.py
+++ test/test_update_version.py
@@ -7,14 +7,13 @@ from version_updater.update_version import VersionUpdater, UpdateMode
 
 
 class FakeEvent(VersionUpdater):
-    def __init__(self, current_version, new_version):
-        super().__init__(current_version, new_version, mode=UpdateMode.Default)
+    def __init__(self, current_version, new_version, mode=UpdateMode.Default):
+        super().__init__(current_version, new_version, mode)
 
 
 def test_add_new_fields_not_in_current():
     """
-    If a field of new_version is not present in current_version,
-    it should be added to current_version with its value set to the value from new_version.
+    Verify if fields in new_version and its values are added to current_version if fields not in current_version.
     :return:
     """
     current_version = load_yaml("test_data_2.yaml")
@@ -35,8 +34,7 @@ def test_add_new_fields_not_in_current():
 
 def test_new_version_in_current_version():
     """
-    If a field of new_version is present in current_version,
-    it should keep the value from current_version.
+    Verify if values in current_version are kept when fields are both in new_version and current_version.
     :return:
     """
     current_version = load_yaml("test_data_1.yaml")
@@ -55,8 +53,7 @@ def test_new_version_in_current_version():
 
 def test_current_version_field_not_in_new_version():
     """
-    If a field of current_version is not present in new_version,
-    it should be removed from current_version.
+    Verify if current_version fields are removed when fields are not in new_version.
     :return:
     """
     current_version = load_yaml("test_data_1.yaml")
@@ -75,6 +72,39 @@ def test_current_version_field_not_in_new_version():
     )
 
 
+def test_simple_update_replace_value_only():
+    """
+    Verify if it only replaces the values of the current_version fields with the values from new_version;
+    No fields are removed from or added to the current_version when mode is Simple update.
+    :return:
+    """
+    current_version = load_yaml("test_data_1.yaml")
+    new_version = load_yaml("test_data_4.yaml")
+    event = FakeEvent(current_version, new_version, UpdateMode.Simple)
+    assert all(
+        event.contains(
+            [
+                ("origin_offset", [0.2, 0.1, 0.5]),
+                ("auto_check", True),
+                ("max_speed", 0.25),
+            ]
+        )
+    ) and not all(
+        event.contains(
+            [
+                (
+                    "move",
+                    {
+                        "max_error": [0.005, 0.005, 0.005],
+                        "max_speed": 0.15,
+                        "max_accel": 0.30,
+                    },
+                )
+            ]
+        )
+    )
+
+
 def load_yaml(file_name: str) -> dict:
     file = pathlib.Path().cwd() / "test" / "test_data" / file_name
     with file.open(mode="r") as fid:
@@ -82,5 +112,4 @@ def load_yaml(file_name: str) -> dict:
 
 
 if __name__ == "__main__":
-    print(pathlib.Path().cwd())
     pytest.main()
